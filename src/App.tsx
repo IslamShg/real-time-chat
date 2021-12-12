cd6663f288cedbd1bd5c1a54c2b68691023af8f7
Islam Shagaew <islamshg@mail.ru>
2021-12-12 18:34:27 +0300

    eslint and prettierr configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import React from "react"
-import "./styles/index.scss"
+import React from 'react'
+import './styles/index.scss'
 
 const App = () => {
-  const islam: number = 123
-  return (
-    <div>
-      Env: {process.env.NODE_ENV}3
-    </div>
-  )
+  return <div>Env: {process.env.NODE_ENV}</div>
 }
 
 export default App
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,16 @@
 const Dotenv = require("dotenv-webpack")
 const path = require("path")
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
 
 module.exports = {
   mode: "development",
   devtool: "cheap-module-source-map",
   devServer: {
     port: 3000,
+    hot: true,
   },
-  plugins: [new Dotenv({ path: path.resolve(__dirname, "..", ".env.dev") })],
+  plugins: [
+    new Dotenv({ path: path.resolve(__dirname, "..", ".env.dev") }),
+    new ReactRefreshWebpackPlugin(),
+  ],
 }
